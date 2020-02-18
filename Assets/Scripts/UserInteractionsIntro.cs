6d5e1134378ff137e28b568db276120b321839f2
Ian Swire <ian@ianswire.com>
2020-02-18 14:49:48 -0500

    Session 1
    Sets up Mirror and creates spawn points for a player to randomly spawn at

---
+++ Assets/Scripts/UserInteractionsIntro.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UserInteractionsIntro : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    public void HelloWorld()
+    {
+        text.text = "Hello World!";
+    }
+}
